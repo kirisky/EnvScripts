c56b84ef67506760de146f3ac76509021d289378
kirisky <youandox@gmail.com>
2020-06-11 23:49:54 +0300

    add whiptail for shell dialog add zsh and oh-my-zsh checking

--- fedora/ohmyzsh.sh
+++
@@ -1,9 +0,0 @@
-
-sudo dnf -y install zsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# 
-# Plugins Repo 
-# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins
-#
--- installationOnFedora_whiptail.sh
+++ installationOnFedora_whiptail.sh
@@ -18,7 +18,7 @@ dockerHostString="export DOCKER_HOST=unix:///run/user/$(id -u)/docker.sock"
 #
 # Aquire scripts
 #
-scripts=$(ls $basePath | grep "\.sh")
+scripts=($(ls $basePath | grep "\.sh"))
 
 #
 # Aquire current user's bin folder
@@ -46,6 +46,33 @@ source ./common/16Color
 ############################# checking if zsh and oh-my-zsh has been installed on this machine #########################
 source ./common/zsh_omz_installation
 check_zsh_and_omz
+
+
+############################# Launch Whiptail Dialog  #######################################################################
+#
+# Launch whiptail diglog and get selected options
+#
+# Reference
+# https://saveriomiroddi.github.io/Shell-scripting-adventures-part-3/#check-list
+#
+title='Fedora Dev-Env Installation'
+message='What apps do you want to install?'
+scriptIndex=1
+whiptailString=""
+for element in ${scripts[@]}
+do
+    whiptailString+="$scriptIndex $element OFF "
+    scriptIndex=$[$scriptIndex + 1]
+done
+selected_options=($(whiptail --separate-output --title "$title" --checklist "$message" 20 36 10 $whiptailString 3>&1 1>&2 2>&3))
+
+tempArray=()
+for i in ${!selected_options[@]}
+do
+    tempArray+=(${scripts[ ${selected_options[i]}-1 ]})
+done
+
+scripts=(${tempArray[@]})
 
 
 ############################# executing logic ##########################################################################
