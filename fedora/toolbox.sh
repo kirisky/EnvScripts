d5ef86c989e3434e9fb61bdc8cc16e9fc2d719f9
kirisky <youandox@gmail.com>
2020-06-12 15:53:11 +0300

    add comment for toolbox

--- fedora/toolbox.sh
+++ fedora/toolbox.sh
@@ -1,1 +1,9 @@
+
+#################################### toolbox ########################################
+#
+# a virtual container for test, like a sandbox
+#
+# Reference
+# https://docs.fedoraproject.org/en-US/fedora-silverblue/toolbox/
+#
 sudo dnf -y install toolbox
