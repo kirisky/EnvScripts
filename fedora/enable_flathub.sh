27ad617cd4f95a997c0e1f45f98205a6fff5abdf
kirisky <youandox@gmail.com>
2020-06-19 15:26:49 +0300

    Add some comments

--- fedora/enable_flathub.sh
+++ fedora/enable_flathub.sh
@@ -1,2 +1,40 @@
 # add flathub to flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+#
+# IntelliJ IDEA Ultimate
+# flatpak install flathub com.jetbrains.IntelliJ-IDEA-Ultimate
+# flatpak run com.jetbrains.IntelliJ-IDEA-Ultimate
+#
+
+#
+# XMind 8
+# flatpak install flathub net.xmind.XMind8
+# flatpak run net.xmind.XMind8
+#
+
+#
+# Mendeley
+# flatpak install flathub com.elsevier.MendeleyDesktop
+# flatpak run com.elsevier.MendeleyDesktop
+#
+
+#
+# VLC
+# flatpak install flathub org.videolan.VLC
+# flatpak run org.videolan.VLC
+# 
+
+# 
+# OBS Studio
+# flatpak install flathub com.obsproject.Studio
+# flatpak run com.obsproject.Studio
+# 
+
+# 
+# Postman
+# flatpak install flathub com.getpostman.Postman
+# flatpak run com.getpostman.Postman
+#
+
+
