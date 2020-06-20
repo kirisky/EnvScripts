bcb4e70e37f6b6edd82c1737827d07e86b278758
andong <kirisky>
2020-06-21 00:43:50 +0300

    added a few of Chinese fonts

--- fedora/fonts.sh
+++ fedora/fonts.sh
@@ -1,5 +1,8 @@
 
 sudo dnf install wqy-microhei-fonts \
+wqy-zenhei-fonts.noarch \
+wqy-unibit-fonts.noarch \
+wqy-bitmap-fonts.noarch \
 google-noto-sans-tc-fonts-20190416-6.fc32.noarch \
 google-noto-serif-tc-fonts-20190416-6.fc32.noarch \
 google-noto-serif-sc-fonts-20190416-6.fc32.noarch \
