36f0c1f0c1ebe20b7cfdae09e0bbbdb953730ef8
kirisky <youandox@gmail.com>
2020-06-21 21:37:31 +0300

    Add git global configurations

---
+++ fedora/gitconfig.sh
@@ -0,0 +1,10 @@
+
+# global info
+git config --global user.name "kirisky"
+git config --global user.email youandox@gmail.com
+
+# global alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
